c17534191d2a6452d4d6dbab89590a9787aafe4d
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-08-09 19:46:58 -0500

    Fixed minor bug removed x on jsx for Calculator import by accident

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Nav from "./components/nav-bar.jsx";
+import Carousel from "./components/carousel.jsx";
+import Last from "./components/last.jsx";
+import First from "./components/first.jsx";
+import Footer from "./components/footer.jsx";
+import Calculator from "./components/calc.jsx"
