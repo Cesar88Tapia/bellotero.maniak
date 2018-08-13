bff614975553d3248011de20e4074abb466e67a1
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-08-13 17:29:52 -0500

    styled buttons

--- client/src/components/nav-bar.jsx
+++ client/src/components/nav-bar.jsx
@@ -6,12 +6,12 @@ export default class Header extends Component {
     return (
     <div className='NavBar'>
         <div className="menu"><img src="https://i.imgur.com/hHs8P2k.png"/>
-        <button>Feature</button>
-        <button>Solutions</button>
-        <button>Stories</button>
-        <button>Partners</button>
-        <button>About</button>
-        <button>Blog</button>
+        <button className="main">Feature</button>
+        <button className="resolved">Solutions</button>
+        <button className="people">Stories</button>
+        <button className="associate">Partners</button>
+        <button className="info">About</button>
+        <button className="blogging">Blog</button>
         <button className='top_request'>Request a demo</button>
         <button className="logIn">Log In</button></div>
         <img  className='NavBar_Img' src="https://i.imgur.com/fe0ReEm.png"/>
